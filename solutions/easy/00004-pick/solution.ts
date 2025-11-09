5e235ed86183539d0e04dfe9e6d1797f2103547f
Akira Takagi <tkgakrwork@gmail.com>
2025-11-09 16:50:57 +0900

    start challenge #4 - pick

---
+++ solutions/easy/00004-pick/solution.ts
@@ -0,0 +1,32 @@
+/*
+ * 4 - pick
+ * Difficulty: easy
+ */
+
+/* _____________ Your Code Here _____________ */
+type MyPick<T, K> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Expected1, MyPick<Todo, 'title'>>>,
+  Expect<Equal<Expected2, MyPick<Todo, 'title' | 'completed'>>>,
+  // @ts-expect-error
+  MyPick<Todo, 'title' | 'completed' | 'invalid'>,
+]
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+interface Expected1 {
+  title: string
+}
+
+interface Expected2 {
+  title: string
+  completed: boolean
+}

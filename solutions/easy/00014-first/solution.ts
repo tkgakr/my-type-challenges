c2b6136792daeba0545bb156b55f1b61e5abaa51
Akira Takagi <tkgakrwork@gmail.com>
2025-11-12 22:26:11 +0900

    start challenge #14 - First of Array

---
+++ solutions/easy/00014-first/solution.ts
@@ -0,0 +1,24 @@
+/*
+ * 14 - first
+ * Difficulty: easy
+ */
+
+/* _____________ Your Code Here _____________ */
+type First<T extends any[]> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<First<[3, 2, 1]>, 3>>,
+  Expect<Equal<First<[() => 123, { a: string }]>, () => 123>>,
+  Expect<Equal<First<[]>, never>>,
+  Expect<Equal<First<[undefined]>, undefined>>,
+]
+
+type errors = [
+  // @ts-expect-error
+  First<'notArray'>,
+  // @ts-expect-error
+  First<{ 0: 'arrayLike' }>,
+]

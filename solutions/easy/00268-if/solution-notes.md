926c3ed1e6743f6f6bb106d018d4b348b746aeb3
Akira Takagi <tkgakrwork@gmail.com>
2025-11-16 21:35:11 +0900

    end challenge #268 If

--- solutions/easy/00268-if/solution-notes.md
+++ solutions/easy/00268-if/solution-notes.md
@@ -10,34 +10,46 @@
 ## 解法
 
 ### アプローチ
-<!-- ここに解法のアプローチを記述 -->
+
+- 判定値 `C` が boolean リテラルであることを前提にし、`C extends true ? T : F` の条件付き型で返り値を選択する。
+- 追加で、`C` に制約を設けない発展解として、falsy 値を網羅するユニオン型 `Falsy` を用意し、`C` が falsy かつ数値リテラルでない場合に `F` を返す条件分岐を実装した。
 
 ### 実装のポイント
-<!-- 重要な実装ポイントを記述 -->
+
+- `If` 型では `C extends boolean` とすることで、問題文の前提を満たさない入力に対して型エラーを発生させるようにした。
+- `MyIf` 型では falsy 値のユニオンを定義し、数値リテラルと `number` の区別を `number extends C` 判定で行うことで、`NaN` のように型で表現できない値を安全に扱えるようにした。
 
 ## 使用した型機能
 
-- [ ] Generics
-- [ ] Conditional Types (`T extends U ? X : Y`)
+- [x] Generics
+- [x] Conditional Types (`T extends U ? X : Y`)
 - [ ] Template Literal Types
 - [ ] Mapped Types (`{ [K in keyof T]: ... }`)
 - [ ] Type Inference (`infer`)
 - [ ] Recursive Types
 - [ ] Utility Types
 - [ ] Index Access Types
-- [ ] Union Types
+- [x] Union Types
 - [ ] Intersection Types
 
 ## 学習メモ
 
 ### 新しく学んだこと
-<!-- 新しい発見や学びを記述 -->
+
+- リテラル型は、値で定義された「型」であって、「値」ではない
+- NaN は数値リテラル型としては表現できず、型としては `number` に丸められる
 
 ### つまずいたポイント
-<!-- 難しかった部分とその解決方法 -->
+
+- 単純な問題の答えについては自力で解けた
+- truthy な型、falsy な型を区別するにあたって、NaN が型としては表現できないこと
 
 ### 参考リンク
-<!-- 参考にした資料のリンク -->
+
+[truthyな値、falsyな値 | TypeScript入門『サバイバルTypeScript』](https://typescriptbook.jp/reference/values-types-variables/truthy-falsy-values)
+
+[リテラル型 (literal type) | TypeScript入門『サバイバルTypeScript』](https://typescriptbook.jp/reference/values-types-variables/literal-types)
+
 
 ---
 *Generated at 2025-11-16 20:17:15*
--- solutions/easy/00268-if/solution.ts
+++ solutions/easy/00268-if/solution.ts
@@ -4,8 +4,24 @@
  */
 
 /* _____________ Your Code Here _____________ */
-type If<C, T, F> = any
+// 問題文には「条件値`C` は`true`か`false`のどちらかであることが期待」と書いてあるので、
+// 条件値 C が true または false であることを制約する
+type If<C extends boolean, T, F> = C extends true ? T : F
 
+// [発展]
+// 条件値 C に制約を持たせず、falsy であれば F、truthy であれば T を返す型を作ってみる
+// まずは、falsy をすべて集めたユニオン型を作る
+// (JavaScript では空配列や空オブジェクトは truthyなので注意)
+// https://typescriptbook.jp/reference/values-types-variables/truthy-falsy-values
+type Falsy = false | 0 | 0n | '' | null | undefined // このユニオン型に `NaN` を含めようとすると、型ではないと怒られる
+// NaN はリテラル型としては表現できず、型レベルでは number に丸められるため、
+// 「NaN を含む幅広い数値型」と「真偽判定できる具体的な数値リテラル型」を区別する
+// そのため、number が C に代入可能かどうかで分岐する
+type MyIf<C, T, F> = C extends Falsy
+  ? F
+  : C extends number
+    ? (number extends C ? F : T)
+    : T
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
@@ -13,7 +29,22 @@ type cases = [
   Expect<Equal<If<true, 'a', 'b'>, 'a'>>,
   Expect<Equal<If<false, 'a', 2>, 2>>,
   Expect<Equal<If<boolean, 'a', 2>, 'a' | 2>>,
+  Expect<Equal<MyIf<true, 'a', 'b'>, 'a'>>,
+  Expect<Equal<MyIf<false, 'a', 2>, 2>>,
+  Expect<Equal<MyIf<boolean, 'a', 2>, 'a' | 2>>,
+  Expect<Equal<MyIf<'hoge', 'a', 'b'>, 'a'>>,
+  Expect<Equal<MyIf<'', 'a', 'b'>, 'b'>>,
+  Expect<Equal<MyIf<[], 'a', 'b'>, 'a'>>,
+  Expect<Equal<MyIf<{}, 'a', 'b'>, 'a'>>,
+  Expect<Equal<MyIf<null, 'a', 'b'>, 'b'>>,
+  Expect<Equal<MyIf<number, 'a', 'b'>, 'b'>>,
 ]
 
 // @ts-expect-error
 type error = If<null, 'a', 'b'>
+// @ts-expect-error
+type error2 = If<[], 'a', 'b'>
+
+type truthy1 = MyIf<'hoge', 'a', 'b'>
+type truthy2 = MyIf<[], 'a', 'b'>
+type falsy1 = MyIf<null, 'a', 'b'>

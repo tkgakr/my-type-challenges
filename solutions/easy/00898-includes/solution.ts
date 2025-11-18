5e1b64a071627b527a6e4495ff3d2be8745475d4
Akira Takagi <tkgakrwork@gmail.com>
2025-11-18 23:33:05 +0900

    wip: challenge #898

--- solutions/easy/00898-includes/solution.ts
+++ solutions/easy/00898-includes/solution.ts
@@ -4,7 +4,33 @@
  */
 
 /* _____________ Your Code Here _____________ */
-type Includes<T extends readonly any[], U> = any
+// 配列を先頭と残りに分離して、先頭がUと一致するかを判定する
+// 一致する場合はtrue、そうでない場合は再帰的に残りの配列を判定する
+// →このやり方は配列内の型とUの型が一致してなければだめだった。
+// type Includes<T extends readonly any[], U> = T extends [infer First, ...infer Rest]
+//   ? First extends U
+//     ? true
+//     : Includes<Rest, U>
+//   : false
+
+// Indexed Access 型を使用して、T を ユニオンに変換する
+// ユニオン型に変換した各要素に対して条件を適用する
+// Distributive Conditional Types を使って、Uがユニオンのいずれかの要素を満たすかを判定する
+// → これも配列内の型とUの型が一致してなければだめだった。
+// type Includes<T extends readonly any[], U> = U extends T[number] ? true : false
+
+// 双方向に型の厳密一致を判定する型を作成
+type IsEqual<X, Y> = (<T>() => T extends X ? 1 : 2) extends (<T>() => T extends Y ? 1 : 2)
+  ? (<T>() => T extends Y ? 1 : 2) extends (<T>() => T extends X ? 1 : 2)
+    ? true
+    : false
+  : false
+
+type Includes<T extends readonly any[], U> = T extends [infer First, ...infer Rest]
+  ? IsEqual<First, U> extends true
+    ? true
+    : Includes<Rest, U>
+  : false
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'

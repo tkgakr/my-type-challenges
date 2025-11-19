27238d97615325f5e96a4d2308aa8d9dc01f2162
Akira Takagi <tkgakrwork@gmail.com>
2025-11-19 22:02:42 +0900

    start challenge #3057 Push

---
+++ solutions/easy/03057-push/solution.ts
@@ -0,0 +1,23 @@
+/*
+ * 3057 - push
+ * Difficulty: easy
+ */
+
+/* _____________ Your Code Here _____________ */
+type Push<T, U> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Push<[], 1>, [1]>>,
+  Expect<Equal<Push<[1, 2], '3'>, [1, 2, '3']>>,
+  Expect<Equal<Push<['1', 2, '3'], boolean>, ['1', 2, '3', boolean]>>,
+]
+
+type errors = [
+  // @ts-expect-error
+  Expect<Equal<Push<number[], string>, string[]>>,
+  // @ts-expect-error
+  Expect<Equal<Push<string[], number>, [string, number]>>,
+]

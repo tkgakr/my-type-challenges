5ae9b40ab3b7c9843d4ec11a5992efa43b57f045
Akira Takagi <tkgakrwork@gmail.com>
2025-11-20 21:30:24 +0900

    start challenge #3060 Unshift

---
+++ solutions/easy/03060-unshift/solution.ts
@@ -0,0 +1,16 @@
+/*
+ * 3060 - unshift
+ * Difficulty: easy
+ */
+
+/* _____________ Your Code Here _____________ */
+type Unshift<T, U> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Unshift<[], 1>, [1]>>,
+  Expect<Equal<Unshift<[1, 2], 0>, [0, 1, 2]>>,
+  Expect<Equal<Unshift<['1', 2, '3'], boolean>, [boolean, '1', 2, '3']>>,
+]

d0c6d70dbe962d0790d98954434c475964a3e5ea
Akira Takagi <tkgakrwork@gmail.com>
2025-11-23 11:14:59 +0900

    start challenge #8 Readonly 2

---
+++ solutions/medium/00008-readonly-2/solution.ts
@@ -0,0 +1,38 @@
+/*
+ * 8 - readonly-2
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type MyReadonly2<T, K> = any
+
+/* _____________ Test Cases _____________ */
+import type { Alike, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Alike<MyReadonly2<Todo1>, Readonly<Todo1>>>,
+  Expect<Alike<MyReadonly2<Todo1, 'title' | 'description'>, Expected>>,
+  Expect<Alike<MyReadonly2<Todo2, 'title' | 'description'>, Expected>>,
+  Expect<Alike<MyReadonly2<Todo2, 'description' >, Expected>>,
+]
+
+// @ts-expect-error
+type error = MyReadonly2<Todo1, 'title' | 'invalid'>
+
+interface Todo1 {
+  title: string
+  description?: string
+  completed: boolean
+}
+
+interface Todo2 {
+  readonly title: string
+  description?: string
+  completed: boolean
+}
+
+interface Expected {
+  readonly title: string
+  readonly description?: string
+  completed: boolean
+}

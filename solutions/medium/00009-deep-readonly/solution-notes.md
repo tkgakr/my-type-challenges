6ea16ec239daf2d9ed55b2a0f73682102c65faa7
Akira Takagi <tkgakrwork@gmail.com>
2025-11-24 21:45:16 +0900

    end challenge #9 Deep Readonly

--- solutions/medium/00009-deep-readonly/solution-notes.md
+++ solutions/medium/00009-deep-readonly/solution-notes.md
@@ -10,34 +10,47 @@
 ## 解法
 
 ### アプローチ
-<!-- ここに解法のアプローチを記述 -->
+
+1. Conditional Types を利用してユニオンを分配評価させ、個々の要素ごとに readonly 変換を適用する。
+2. 関数型はそのまま返し、オブジェクト型にのみ再帰的な Mapped Type を適用して深い階層まで readonly 化する。
+3. オブジェクト判定は `T extends { [k in string]: any }` と `keyof T extends never` の 2 パターンを比較・検証した。
 
 ### 実装のポイント
-<!-- 重要な実装ポイントを記述 -->
+
+- `T extends (...args: any[]) => any ? T : ...` で関数を早期 return。
+- オブジェクト判定後に `{ readonly [P in keyof T]: DeepReadonly<T[P]> }` を再帰的に適用。
+- `DeepReadonly2` では `keyof T extends never` を利用してオブジェクト/非オブジェクトを見分ける別解を提示。
 
 ## 使用した型機能
 
-- [ ] Generics
-- [ ] Conditional Types (`T extends U ? X : Y`)
+- [x] Generics
+- [x] Conditional Types (`T extends U ? X : Y`)
 - [ ] Template Literal Types
-- [ ] Mapped Types (`{ [K in keyof T]: ... }`)
+- [x] Mapped Types (`{ [K in keyof T]: ... }`)
 - [ ] Type Inference (`infer`)
-- [ ] Recursive Types
+- [x] Recursive Types
 - [ ] Utility Types
 - [ ] Index Access Types
-- [ ] Union Types
+- [x] Union Types
 - [ ] Intersection Types
 
 ## 学習メモ
 
 ### 新しく学んだこと
-<!-- 新しい発見や学びを記述 -->
+
+- Distributive Conditional Types を利用するとユニオン構成要素ごとに再帰を適用できる。
+- `keyof T extends never` をオブジェクト判定に使うパターンで edge case を減らせる。
+- 条件型で複数種類の関数型を判定する方法
 
 ### つまずいたポイント
-<!-- 難しかった部分とその解決方法 -->
+
+- オブジェクト型かどうかの判定
+- 関数型かどうかの判定
+- ユニオンの条件型の取り扱い
 
 ### 参考リンク
-<!-- 参考にした資料のリンク -->
+
+[Distributive Conditional Types | TypeScript入門『サバイバルTypeScript』](https://typescriptbook.jp/reference/type-reuse/conditional-types/distributive-conditional-types)
 
 ---
 *Generated at 2025-11-24 20:03:26*
--- solutions/medium/00009-deep-readonly/solution.ts
+++ solutions/medium/00009-deep-readonly/solution.ts
@@ -4,7 +4,22 @@
  */
 
 /* _____________ Your Code Here _____________ */
-type DeepReadonly<T> = any
+
+// ユニオンでも個別に評価されるよう条件付き型で判定する
+// `T extends ... ? ... : ...` という条件付き型の形にした場合、
+// T がユニオン型だと構成要素ごとに分配される (distributive conditional types) 
+type DeepReadonly<T> = T extends (...args: any[]) => any // (ユニオン型の各要素の対して)関数か？
+  ? T //関数はそのまま返す
+  : T extends { [k in string]: any } // オブジェクトか？(別解)
+    ? { readonly [P in keyof T]: DeepReadonly<T[P]> } // オブジェクトなら再帰的に readonly する
+    : T // それ以外はそのまま返す
+
+// 【別解】オブジェクトかどうかの判定に `key of T` で T がプロパティをもつか否かを判定する
+type DeepReadonly2<T> = T extends Function // (ユニオン型の各要素の対して)関数か？
+  ? T //関数はそのまま返す
+  : keyof T extends never // 非オブジェクトか？
+    ? T // 非オブジェクトはそのまま返す
+    : { readonly [P in keyof T]: DeepReadonly<T[P]> } // オブジェクトなら再帰的に readonly する
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
@@ -12,6 +27,8 @@ import type { Equal, Expect } from '@type-challenges/utils'
 type cases = [
   Expect<Equal<DeepReadonly<X1>, Expected1>>,
   Expect<Equal<DeepReadonly<X2>, Expected2>>,
+  Expect<Equal<DeepReadonly2<X1>, Expected1>>,
+  Expect<Equal<DeepReadonly2<X2>, Expected2>>,
 ]
 
 type X1 = {
@@ -63,3 +80,22 @@ type Expected1 = {
 }
 
 type Expected2 = { readonly a: string } | { readonly b: number }
+
+// 問題文より
+type X = { 
+  x: { 
+    a: 1
+    b: 'hi'
+  }
+  y: 'hey'
+}
+
+type Expected = { 
+  readonly x: { 
+    readonly a: 1
+    readonly b: 'hi'
+  }
+  readonly y: 'hey' 
+}
+// IDEでホバーして確認
+type Todo = DeepReadonly<X> // should be same as `Expected`

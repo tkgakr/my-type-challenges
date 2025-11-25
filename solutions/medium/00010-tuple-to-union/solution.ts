f95750112b6e85efffca6fef78d7c56a28cf1395
Akira Takagi <tkgakrwork@gmail.com>
2025-11-25 22:59:30 +0900

    start challenge #10 Tuple to Union

---
+++ solutions/medium/00010-tuple-to-union/solution.ts
@@ -0,0 +1,15 @@
+/*
+ * 10 - tuple-to-union
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type TupleToUnion<T> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<TupleToUnion<[123, '456', true]>, 123 | '456' | true>>,
+  Expect<Equal<TupleToUnion<[123]>, 123>>,
+]

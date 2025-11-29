4d2a239938af9924eace3854aa30badca1692f5e
Akira Takagi <tkgakrwork@gmail.com>
2025-11-29 21:08:25 +0900

    start challenge #16 Pop

---
+++ solutions/medium/00016-pop/solution.ts
@@ -0,0 +1,16 @@
+/*
+ * 16 - pop
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type Pop<T extends any[]> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
+  Expect<Equal<Pop<['a', 'b', 'c', 'd']>, ['a', 'b', 'c']>>,
+  Expect<Equal<Pop<[]>, []>>,
+]

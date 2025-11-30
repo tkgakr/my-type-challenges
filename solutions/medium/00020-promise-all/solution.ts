ca2698825c987447981a673e7b857860e7862cd8
Akira Takagi <tkgakrwork@gmail.com>
2025-11-30 20:47:49 +0900

    start challenge #20 Promise.all

---
+++ solutions/medium/00020-promise-all/solution.ts
@@ -0,0 +1,24 @@
+/*
+ * 20 - promise-all
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+declare function PromiseAll(values: any): any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+const promiseAllTest1 = PromiseAll([1, 2, 3] as const)
+const promiseAllTest2 = PromiseAll([1, 2, Promise.resolve(3)] as const)
+const promiseAllTest3 = PromiseAll([1, 2, Promise.resolve(3)])
+const promiseAllTest4 = PromiseAll<Array<number | Promise<number>>>([1, 2, 3])
+const promiseAllTest5 = PromiseAll<(number | Promise<string>)[]>([1, 2, Promise.resolve('3')])
+
+type cases = [
+  Expect<Equal<typeof promiseAllTest1, Promise<[1, 2, 3]>>>,
+  Expect<Equal<typeof promiseAllTest2, Promise<[1, 2, number]>>>,
+  Expect<Equal<typeof promiseAllTest3, Promise<[number, number, number]>>>,
+  Expect<Equal<typeof promiseAllTest4, Promise<number[]>>>,
+  Expect<Equal<typeof promiseAllTest5, Promise<(number | string)[]>>>,
+]

e8d68ecf4d7fe81eb16d6d4258ea3f77708b623f
Akira Takagi <tkgakrwork@gmail.com>
2025-12-01 21:09:23 +0900

    start challenge #62 Type Lookup

---
+++ solutions/medium/00062-type-lookup/solution.ts
@@ -0,0 +1,28 @@
+/*
+ * 62 - type-lookup
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type LookUp<U, T> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+interface Cat {
+  type: 'cat'
+  breeds: 'Abyssinian' | 'Shorthair' | 'Curl' | 'Bengal'
+}
+
+interface Dog {
+  type: 'dog'
+  breeds: 'Hound' | 'Brittany' | 'Bulldog' | 'Boxer'
+  color: 'brown' | 'white' | 'black'
+}
+
+type Animal = Cat | Dog
+
+type cases = [
+  Expect<Equal<LookUp<Animal, 'dog'>, Dog>>,
+  Expect<Equal<LookUp<Animal, 'cat'>, Cat>>,
+]

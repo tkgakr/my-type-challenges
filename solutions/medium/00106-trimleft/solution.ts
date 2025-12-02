8ca7c35418e8f1ca4287b54e9b25d93478a58ef5
Akira Takagi <tkgakrwork@gmail.com>
2025-12-02 23:11:07 +0900

    start challenge #106 TrimLeft

---
+++ solutions/medium/00106-trimleft/solution.ts
@@ -0,0 +1,20 @@
+/*
+ * 106 - trimleft
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type TrimLeft<S extends string> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<TrimLeft<'str'>, 'str'>>,
+  Expect<Equal<TrimLeft<' str'>, 'str'>>,
+  Expect<Equal<TrimLeft<'     str'>, 'str'>>,
+  Expect<Equal<TrimLeft<'     str     '>, 'str     '>>,
+  Expect<Equal<TrimLeft<'   \n\t foo bar '>, 'foo bar '>>,
+  Expect<Equal<TrimLeft<''>, ''>>,
+  Expect<Equal<TrimLeft<' \n\t'>, ''>>,
+]

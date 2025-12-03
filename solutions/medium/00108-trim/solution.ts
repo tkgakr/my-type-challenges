688498001ae842b4645f4a602fa85276f4a96440
Akira Takagi <tkgakrwork@gmail.com>
2025-12-03 22:29:57 +0900

    start challenge #108 Trim

---
+++ solutions/medium/00108-trim/solution.ts
@@ -0,0 +1,21 @@
+/*
+ * 108 - trim
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type Trim<S extends string> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Trim<'str'>, 'str'>>,
+  Expect<Equal<Trim<' str'>, 'str'>>,
+  Expect<Equal<Trim<'     str'>, 'str'>>,
+  Expect<Equal<Trim<'str   '>, 'str'>>,
+  Expect<Equal<Trim<'     str     '>, 'str'>>,
+  Expect<Equal<Trim<'   \n\t foo bar \t'>, 'foo bar'>>,
+  Expect<Equal<Trim<''>, ''>>,
+  Expect<Equal<Trim<' \n\t '>, ''>>,
+]

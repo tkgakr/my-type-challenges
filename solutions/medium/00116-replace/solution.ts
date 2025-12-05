d9f2c8ba0bc03e61b8d894c2c5ac7f0b828dbd0c
Akira Takagi <tkgakrwork@gmail.com>
2025-12-05 22:22:33 +0900

    start challenge #116 Replace

---
+++ solutions/medium/00116-replace/solution.ts
@@ -0,0 +1,19 @@
+/*
+ * 116 - replace
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type Replace<S extends string, From extends string, To extends string> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Replace<'foobar', 'bar', 'foo'>, 'foofoo'>>,
+  Expect<Equal<Replace<'foobarbar', 'bar', 'foo'>, 'foofoobar'>>,
+  Expect<Equal<Replace<'foobarbar', '', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<Replace<'foobarbar', 'bar', ''>, 'foobar'>>,
+  Expect<Equal<Replace<'foobarbar', 'bra', 'foo'>, 'foobarbar'>>,
+  Expect<Equal<Replace<'', '', ''>, ''>>,
+]

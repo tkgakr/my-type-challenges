620f406d84bbff9ddc034ec97625bbebbf8089fd
Akira Takagi <tkgakrwork@gmail.com>
2025-12-06 17:47:08 +0900

    start challenge #191 AppendArgument

---
+++ solutions/medium/00191-append-argument/solution.ts
@@ -0,0 +1,23 @@
+/*
+ * 191 - append-argument
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type AppendArgument<Fn, A> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type Case1 = AppendArgument<(a: number, b: string) => number, boolean>
+type Result1 = (a: number, b: string, x: boolean) => number
+
+type Case2 = AppendArgument<() => void, undefined>
+type Result2 = (x: undefined) => void
+
+type cases = [
+  Expect<Equal<Case1, Result1>>,
+  Expect<Equal<Case2, Result2>>,
+  // @ts-expect-error
+  AppendArgument<unknown, undefined>,
+]

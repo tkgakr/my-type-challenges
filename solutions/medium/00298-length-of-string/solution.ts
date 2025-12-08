552b8c7a1fc6898dc729cb113e84888d257724ab
Akira Takagi <tkgakrwork@gmail.com>
2025-12-08 22:32:43 +0900

    start challenge #298 LengthOfString

---
+++ solutions/medium/00298-length-of-string/solution.ts
@@ -0,0 +1,17 @@
+/*
+ * 298 - length-of-string
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type LengthOfString<S extends string> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<LengthOfString<''>, 0>>,
+  Expect<Equal<LengthOfString<'kumiko'>, 6>>,
+  Expect<Equal<LengthOfString<'reina'>, 5>>,
+  Expect<Equal<LengthOfString<'Sound! Euphonium'>, 16>>,
+]

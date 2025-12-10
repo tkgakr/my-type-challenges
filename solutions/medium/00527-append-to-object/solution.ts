883189b1781fb807a264c4740ee693bf3d0291bd
Akira Takagi <tkgakrwork@gmail.com>
2025-12-10 21:14:45 +0900

    start challenge #527 AppendToObject

---
+++ solutions/medium/00527-append-to-object/solution.ts
@@ -0,0 +1,53 @@
+/*
+ * 527 - append-to-object
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type AppendToObject<T, U, V> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type test1 = {
+  key: 'cat'
+  value: 'green'
+}
+
+type testExpect1 = {
+  key: 'cat'
+  value: 'green'
+  home: boolean
+}
+
+type test2 = {
+  key: 'dog' | undefined
+  value: 'white'
+  sun: true
+}
+
+type testExpect2 = {
+  key: 'dog' | undefined
+  value: 'white'
+  sun: true
+  home: 1
+}
+
+type test3 = {
+  key: 'cow'
+  value: 'yellow'
+  sun: false
+}
+
+type testExpect3 = {
+  key: 'cow'
+  value: 'yellow'
+  sun: false
+  moon: false | undefined
+}
+
+type cases = [
+  Expect<Equal<AppendToObject<test1, 'home', boolean>, testExpect1>>,
+  Expect<Equal<AppendToObject<test2, 'home', 1>, testExpect2>>,
+  Expect<Equal<AppendToObject<test3, 'moon', false | undefined>, testExpect3>>,
+]

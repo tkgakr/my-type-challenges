9c91ba921361d385c4c12f24daaa4b071ff4aa32
Akira Takagi <tkgakrwork@gmail.com>
2025-12-12 22:37:18 +0900

    start challenge #531 StringToUnion

---
+++ solutions/medium/00531-string-to-union/solution.ts
@@ -0,0 +1,17 @@
+/*
+ * 531 - string-to-union
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type StringToUnion<T extends string> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<StringToUnion<''>, never>>,
+  Expect<Equal<StringToUnion<'t'>, 't'>>,
+  Expect<Equal<StringToUnion<'hello'>, 'h' | 'e' | 'l' | 'l' | 'o'>>,
+  Expect<Equal<StringToUnion<'coronavirus'>, 'c' | 'o' | 'r' | 'o' | 'n' | 'a' | 'v' | 'i' | 'r' | 'u' | 's'>>,
+]

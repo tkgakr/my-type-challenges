92f8ef23e1a287efc36773e238dfce49b0246743
Akira Takagi <tkgakrwork@gmail.com>
2025-12-13 20:29:40 +0900

    start challenge #599 Merge

---
+++ solutions/medium/00599-merge/solution.ts
@@ -0,0 +1,27 @@
+/*
+ * 599 - merge
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type Merge<F, S> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type Foo = {
+  a: number
+  b: string
+}
+type Bar = {
+  b: number
+  c: boolean
+}
+
+type cases = [
+  Expect<Equal<Merge<Foo, Bar>, {
+    a: number
+    b: number
+    c: boolean
+  }>>,
+]

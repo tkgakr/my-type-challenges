0739d64be5d8e6a49cafffd0e4eab5a62e069a33
Akira Takagi <tkgakrwork@gmail.com>
2025-12-16 21:09:25 +0900

    start challenge #1042 IsNever

---
+++ solutions/medium/01042-isnever/solution.ts
@@ -0,0 +1,20 @@
+/*
+ * 1042 - isnever
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type IsNever<T> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<IsNever<never>, true>>,
+  Expect<Equal<IsNever<never | string>, false>>,
+  Expect<Equal<IsNever<''>, false>>,
+  Expect<Equal<IsNever<undefined>, false>>,
+  Expect<Equal<IsNever<null>, false>>,
+  Expect<Equal<IsNever<[]>, false>>,
+  Expect<Equal<IsNever<{}>, false>>,
+]

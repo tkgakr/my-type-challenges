c31bb3e225753f83d2220979058a443fef42807c
Akira Takagi <tkgakrwork@gmail.com>
2025-12-20 09:28:29 +0900

    start challenge #2070 DropChar

---
+++ solutions/medium/02070-drop-char/solution.ts
@@ -0,0 +1,21 @@
+/*
+ * 2070 - drop-char
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type DropChar<S, C> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  // @ts-expect-error
+  Expect<Equal<DropChar<'butter fly!', ''>, 'butterfly!'>>,
+  Expect<Equal<DropChar<'butter fly!', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropChar<'butter fly!', '!'>, 'butter fly'>>,
+  Expect<Equal<DropChar<'    butter fly!        ', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropChar<' b u t t e r f l y ! ', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropChar<' b u t t e r f l y ! ', 'b'>, '  u t t e r f l y ! '>>,
+  Expect<Equal<DropChar<' b u t t e r f l y ! ', 't'>, ' b u   e r f l y ! '>>,
+]

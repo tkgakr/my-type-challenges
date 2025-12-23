b31f4af0ef0641cfe5cd87eb535242741236b241
Akira Takagi <tkgakrwork@gmail.com>
2025-12-23 20:57:37 +0900

    start challenge #2688 StartsWith

---
+++ solutions/medium/02688-startswith/solution.ts
@@ -0,0 +1,20 @@
+/*
+ * 2688 - startswith
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type StartsWith<T extends string, U extends string> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<StartsWith<'abc', 'ac'>, false>>,
+  Expect<Equal<StartsWith<'abc', 'ab'>, true>>,
+  Expect<Equal<StartsWith<'abc', 'abc'>, true>>,
+  Expect<Equal<StartsWith<'abc', 'abcd'>, false>>,
+  Expect<Equal<StartsWith<'abc', ''>, true>>,
+  Expect<Equal<StartsWith<'abc', ' '>, false>>,
+  Expect<Equal<StartsWith<'', ''>, true>>,
+]

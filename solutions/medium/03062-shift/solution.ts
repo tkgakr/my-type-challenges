bf9455928989a36097bb896b3676c00675920dee
Akira Takagi <tkgakrwork@gmail.com>
2025-12-27 14:53:04 +0900

    start challenge #3062 Shift

---
+++ solutions/medium/03062-shift/solution.ts
@@ -0,0 +1,19 @@
+/*
+ * 3062 - shift
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type Shift<T> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  // @ts-expect-error
+  Shift<unknown>,
+  Expect<Equal<Shift<[]>, []>>,
+  Expect<Equal<Shift<[1]>, []>>,
+  Expect<Equal<Shift<[3, 2, 1]>, [2, 1]>>,
+  Expect<Equal<Shift<['a', 'b', 'c', 'd']>, ['b', 'c', 'd']>>,
+]

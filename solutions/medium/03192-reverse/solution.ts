c1cc02d7a502be165ccf0c4df286077f945d6a71
Akira Takagi <tkgakrwork@gmail.com>
2025-12-28 10:51:55 +0900

    start challenge #3192 Reverse

---
+++ solutions/medium/03192-reverse/solution.ts
@@ -0,0 +1,23 @@
+/*
+ * 3192 - reverse
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type Reverse<T> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Reverse<[]>, []>>,
+  Expect<Equal<Reverse<['a', 'b']>, ['b', 'a']>>,
+  Expect<Equal<Reverse<['a', 'b', 'c']>, ['c', 'b', 'a']>>,
+]
+
+type errors = [
+  // @ts-expect-error
+  Reverse<'string'>,
+  // @ts-expect-error
+  Reverse<{ key: 'value' }>,
+]

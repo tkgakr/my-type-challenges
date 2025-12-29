52ed51f316df4dfa7e1430f49c3980014f7fffab
Akira Takagi <tkgakrwork@gmail.com>
2025-12-29 14:02:43 +0900

    start challenge #3243 FlattenDepth

---
+++ solutions/medium/03243-flattendepth/solution.ts
@@ -0,0 +1,20 @@
+/*
+ * 3243 - flattendepth
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type FlattenDepth = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<FlattenDepth<[]>, []>>,
+  Expect<Equal<FlattenDepth<[1, 2, 3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<FlattenDepth<[1, [2]]>, [1, 2]>>,
+  Expect<Equal<FlattenDepth<[1, 2, [3, 4], [[[5]]]], 2>, [1, 2, 3, 4, [5]]>>,
+  Expect<Equal<FlattenDepth<[1, 2, [3, 4], [[[5]]]]>, [1, 2, 3, 4, [[5]]]>>,
+  Expect<Equal<FlattenDepth<[1, [2, [3, [4, [5]]]]], 3>, [1, 2, 3, 4, [5]]>>,
+  Expect<Equal<FlattenDepth<[1, [2, [3, [4, [5]]]]], 19260817>, [1, 2, 3, 4, 5]>>,
+]

4ca5a56ca3e2946746fef4e2b09b377f9afe8397
Akira Takagi <tkgakrwork@gmail.com>
2026-01-01 17:16:42 +0900

    start challenge #4179 Flip

---
+++ solutions/medium/04179-flip/solution.ts
@@ -0,0 +1,17 @@
+/*
+ * 4179 - flip
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type Flip<T> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect, NotEqual } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<{ a: 'pi' }, Flip<{ pi: 'a' }>>>,
+  Expect<NotEqual<{ b: 'pi' }, Flip<{ pi: 'a' }>>>,
+  Expect<Equal<{ 3.14: 'pi', true: 'bool' }, Flip<{ pi: 3.14, bool: true }>>>,
+  Expect<Equal<{ val2: 'prop2', val: 'prop' }, Flip<{ prop: 'val', prop2: 'val2' }>>>,
+]

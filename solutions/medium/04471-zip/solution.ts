3e95ba127e10de4fa7d4d84f5fc9a0e5861fab5a
Akira Takagi <tkgakrwork@gmail.com>
2026-01-05 21:27:48 +0900

    start challenge #4471 Zip

---
+++ solutions/medium/04471-zip/solution.ts
@@ -0,0 +1,18 @@
+/*
+ * 4471 - zip
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type Zip<T, U> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Zip<[], []>, []>>,
+  Expect<Equal<Zip<[1, 2], [true, false]>, [[1, true], [2, false]]>>,
+  Expect<Equal<Zip<[1, 2, 3], ['1', '2']>, [[1, '1'], [2, '2']]>>,
+  Expect<Equal<Zip<[], [1, 2, 3]>, []>>,
+  Expect<Equal<Zip<[[1, 2]], [3]>, [[[1, 2], 3]]>>,
+]

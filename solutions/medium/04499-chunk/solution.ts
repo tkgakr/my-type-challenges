bedac4cc8236c3700f83beaba472bcc0dbf47730
Akira Takagi <tkgakrwork@gmail.com>
2026-01-07 22:14:19 +0900

    start challenge #4499 Chunk

---
+++ solutions/medium/04499-chunk/solution.ts
@@ -0,0 +1,19 @@
+/*
+ * 4499 - chunk
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type Chunk = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Chunk<[], 1>, []>>,
+  Expect<Equal<Chunk<[1, 2, 3], 1>, [[1], [2], [3]]>>,
+  Expect<Equal<Chunk<[1, 2, 3], 2>, [[1, 2], [3]]>>,
+  Expect<Equal<Chunk<[1, 2, 3, 4], 2>, [[1, 2], [3, 4]]>>,
+  Expect<Equal<Chunk<[1, 2, 3, 4], 5>, [[1, 2, 3, 4]]>>,
+  Expect<Equal<Chunk<[1, true, 2, false], 2>, [[1, true], [2, false]]>>,
+]

42e72c20ded1d465459aeac0698f9f9cd1feda82
Akira Takagi <tkgakrwork@gmail.com>
2026-01-09 19:02:22 +0900

    start challenge #5117 Without

---
+++ solutions/medium/05117-without/solution.ts
@@ -0,0 +1,16 @@
+/*
+ * 5117 - without
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type Without<T, U> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Without<[1, 2], 1>, [2]>>,
+  Expect<Equal<Without<[1, 2, 4, 1, 5], [1, 2]>, [4, 5]>>,
+  Expect<Equal<Without<[2, 3, 2, 3, 2, 3, 2, 3], [2, 3]>, []>>,
+]

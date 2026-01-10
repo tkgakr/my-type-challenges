6c77be3c4efe803b66eee60960906e18ae7e92a1
Akira Takagi <tkgakrwork@gmail.com>
2026-01-10 18:25:02 +0900

    start challenge #5140 Trunc

---
+++ solutions/medium/05140-trunc/solution.ts
@@ -0,0 +1,23 @@
+/*
+ * 5140 - trunc
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type Trunc = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Trunc<0.1>, '0'>>,
+  Expect<Equal<Trunc<0.2>, '0'>>,
+  Expect<Equal<Trunc<1.234>, '1'>>,
+  Expect<Equal<Trunc<12.345>, '12'>>,
+  Expect<Equal<Trunc<-5.1>, '-5'>>,
+  Expect<Equal<Trunc<'.3'>, '0'>>,
+  Expect<Equal<Trunc<'1.234'>, '1'>>,
+  Expect<Equal<Trunc<'-.3'>, '-0'>>,
+  Expect<Equal<Trunc<'-10.234'>, '-10'>>,
+  Expect<Equal<Trunc<10>, '10'>>,
+]

33d664d6e019b1edc28721f82962d8774ab4681c
Akira Takagi <tkgakrwork@gmail.com>
2026-01-12 20:18:53 +0900

    start challenge #5360 Unique

---
+++ solutions/medium/05360-unique/solution.ts
@@ -0,0 +1,18 @@
+/*
+ * 5360 - unique
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type Unique<T> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Unique<[1, 1, 2, 2, 3, 3]>, [1, 2, 3]>>,
+  Expect<Equal<Unique<[1, 2, 3, 4, 4, 5, 6, 7]>, [1, 2, 3, 4, 5, 6, 7]>>,
+  Expect<Equal<Unique<[1, 'a', 2, 'b', 2, 'a']>, [1, 'a', 2, 'b']>>,
+  Expect<Equal<Unique<[string, number, 1, 'a', 1, string, 2, 'b', 2, number]>, [string, number, 1, 'a', 2, 'b']>>,
+  Expect<Equal<Unique<[unknown, unknown, any, any, never, never]>, [unknown, any, never]>>,
+]

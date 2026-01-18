8c777e37e19d3c6d898c3c17af631519bf687dc3
Akira Takagi <tkgakrwork@gmail.com>
2026-01-18 14:49:35 +0900

    start challenge #9142 CheckRepeatedChars

---
+++ solutions/medium/09142-checkrepeatedchars/solution.ts
@@ -0,0 +1,17 @@
+/*
+ * 9142 - checkrepeatedchars
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type CheckRepeatedChars<T extends string> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<CheckRepeatedChars<'abc'>, false>>,
+  Expect<Equal<CheckRepeatedChars<'abb'>, true>>,
+  Expect<Equal<CheckRepeatedChars<'cbc'>, true>>,
+  Expect<Equal<CheckRepeatedChars<''>, false>>,
+]

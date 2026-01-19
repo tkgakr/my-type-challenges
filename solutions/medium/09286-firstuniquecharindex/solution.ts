18b5c5f47d279d646ed24bc441906a9207609a0d
Akira Takagi <tkgakrwork@gmail.com>
2026-01-19 10:57:47 +0900

    start challenge #9286 FirstUniqueCharIndex

---
+++ solutions/medium/09286-firstuniquecharindex/solution.ts
@@ -0,0 +1,18 @@
+/*
+ * 9286 - firstuniquecharindex
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type FirstUniqueCharIndex<T extends string> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<FirstUniqueCharIndex<'leetcode'>, 0>>,
+  Expect<Equal<FirstUniqueCharIndex<'loveleetcode'>, 2>>,
+  Expect<Equal<FirstUniqueCharIndex<'aabb'>, -1>>,
+  Expect<Equal<FirstUniqueCharIndex<''>, -1>>,
+  Expect<Equal<FirstUniqueCharIndex<'aaa'>, -1>>,
+]

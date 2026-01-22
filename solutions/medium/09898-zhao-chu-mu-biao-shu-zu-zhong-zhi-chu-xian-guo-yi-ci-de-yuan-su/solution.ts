56d66c4b4657e2410f45307431830f3c98b4c7f2
Akira Takagi <tkgakrwork@gmail.com>
2026-01-22 21:33:14 +0900

    start challenge #9898 Appear only once

---
+++ solutions/medium/09898-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su/solution.ts
@@ -0,0 +1,18 @@
+/*
+ * 9898 - zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type FindEles<T extends any[]> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<FindEles<[1, 2, 2, 3, 3, 4, 5, 6, 6, 6]>, [1, 4, 5]>>,
+  Expect<Equal<FindEles<[2, 2, 3, 3, 6, 6, 6]>, []>>,
+  Expect<Equal<FindEles<[1, 2, 3]>, [1, 2, 3]>>,
+  Expect<Equal<FindEles<[1, 2, number]>, [1, 2, number]>>,
+  Expect<Equal<FindEles<[1, 2, number, number]>, [1, 2]>>,
+]

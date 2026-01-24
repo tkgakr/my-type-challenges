5b8f7d591cb5d87ea1310a87d9224c5639ab458e
Akira Takagi <tkgakrwork@gmail.com>
2026-01-24 18:52:39 +0900

    start challenge #10969 Integer

---
+++ solutions/medium/10969-integer/solution.ts
@@ -0,0 +1,25 @@
+/*
+ * 10969 - integer
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type Integer<T> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+let x = 1
+let y = 1 as const
+
+type cases1 = [
+  Expect<Equal<Integer<1>, 1>>,
+  Expect<Equal<Integer<1.1>, never>>,
+  Expect<Equal<Integer<1.0>, 1>>,
+  Expect<Equal<Integer<1.000000000>, 1>>,
+  Expect<Equal<Integer<0.5>, never>>,
+  Expect<Equal<Integer<28.00>, 28>>,
+  Expect<Equal<Integer<28.101>, never>>,
+  Expect<Equal<Integer<typeof x>, never>>,
+  Expect<Equal<Integer<typeof y>, 1>>,
+]

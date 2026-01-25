d96914e891f973d7da88897f028a48be6714c310
Akira Takagi <tkgakrwork@gmail.com>
2026-01-25 15:09:42 +0900

    start challenge #16259 ToPrimitive

---
+++ solutions/medium/16259-to-primitive/solution.ts
@@ -0,0 +1,40 @@
+/*
+ * 16259 - to-primitive
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type ToPrimitive = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type PersonInfo = {
+  name: 'Tom'
+  age: 30
+  married: false
+  addr: {
+    home: '123456'
+    phone: '13111111111'
+  }
+  hobbies: ['sing', 'dance']
+  readonlyArr: readonly ['test']
+  fn: () => any
+}
+
+type ExpectedResult = {
+  name: string
+  age: number
+  married: boolean
+  addr: {
+    home: string
+    phone: string
+  }
+  hobbies: [string, string]
+  readonlyArr: readonly [string]
+  fn: Function
+}
+
+type cases = [
+  Expect<Equal<ToPrimitive<PersonInfo>, ExpectedResult>>,
+]

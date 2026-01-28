f18f0adf0fe87faa2aa5c95926cf11ad31cf8a7a
Akira Takagi <tkgakrwork@gmail.com>
2026-01-28 21:21:58 +0900

    end challenge #18220 Filter

--- solutions/medium/18220-filter/solution.ts
+++ solutions/medium/18220-filter/solution.ts
@@ -4,7 +4,21 @@
  */
 
 /* _____________ Your Code Here _____________ */
-type Filter<T extends any[], P> = []
+// 解法1: アキュムレータを使用しないアプローチ
+type Filter<T extends any[], P> =
+  T extends [infer First, ...infer Rest]
+    ? First extends P
+      ? [First, ...Filter<Rest, P>]
+      : Filter<Rest, P>
+    : []
+
+// 解法2: アキュムレータを使用するアプローチ
+type Filter2<T extends any[], P, Result extends any[] = []> =
+  T extends [infer First, ...infer Rest]
+    ? First extends P
+      ? Filter2<Rest, P, [...Result, First]>
+      : Filter2<Rest, P, Result>
+    : Result
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
@@ -15,4 +29,10 @@ type cases = [
   Expect<Equal<Filter<[0, 1, 2], 2>, [2]>>,
   Expect<Equal<Filter<[0, 1, 2], 0 | 1>, [0, 1]>>,
   Expect<Equal<Filter<[0, 1, 2], Falsy>, [0]>>,
+]
+
+type cases2 = [
+  Expect<Equal<Filter2<[0, 1, 2], 2>, [2]>>,
+  Expect<Equal<Filter2<[0, 1, 2], 0 | 1>, [0, 1]>>,
+  Expect<Equal<Filter2<[0, 1, 2], Falsy>, [0]>>,
 ]

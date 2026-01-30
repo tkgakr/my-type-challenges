d6e4f4931a0b1bde496a021c0ba0c56d15e28adb
Akira Takagi <tkgakrwork@gmail.com>
2026-01-30 22:57:22 +0900

    start challenge #21106 Combination key type

---
+++ solutions/medium/21106-zu-he-jian-lei-xing-combination-key-type/solution.ts
@@ -0,0 +1,18 @@
+/*
+ * 21106 - zu-he-jian-lei-xing-combination-key-type
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+// 实现 Combs
+type Combs<T extends any[]> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type ModifierKeys = ['cmd', 'ctrl', 'opt', 'fn']
+type CaseTypeOne = 'cmd ctrl' | 'cmd opt' | 'cmd fn' | 'ctrl opt' | 'ctrl fn' | 'opt fn'
+
+type cases = [
+  Expect<Equal<Combs<ModifierKeys>, CaseTypeOne>>,
+]

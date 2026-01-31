199823bd9dbf764ec117007b0a53b0e4bb33d8c6
Akira Takagi <tkgakrwork@gmail.com>
2026-01-31 18:30:51 +0900

    end challenge #21220 Permutations of Tuple

--- solutions/medium/21220-permutations-of-tuple/solution.ts
+++ solutions/medium/21220-permutations-of-tuple/solution.ts
@@ -4,7 +4,16 @@
  */
 
 /* _____________ Your Code Here _____________ */
-type PermutationsOfTuple<T extends unknown[]> = any
+// Prev は処理済みの要素配列
+type PermutationsOfTuple<T extends unknown[], Prev extends unknown[] = []> =
+  // T を最初の要素と残りにわける
+  T extends [infer First, ...infer Rest]
+    // 最初の要素を固定して、PrevとRestをあわせた配列(First以外の要素)の順列をもとめる(ユニオンで展開される)
+    ? [First, ...PermutationsOfTuple<[...Prev, ...Rest]>]
+      // 残りの要素がなくなれば再帰終了、まだある場合は、Firstを処理済み要素につめて残りの要素を再帰
+      | (Rest extends []? never : PermutationsOfTuple<Rest, [...Prev, First]>)
+    // 要素がなくなったら再帰終了
+    : []
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect, ExpectFalse } from '@type-challenges/utils'

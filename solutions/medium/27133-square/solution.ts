f4b430b99bf3110315363088c0c0d8f341811afb
Akira Takagi <tkgakrwork@gmail.com>
2026-02-04 23:07:19 +0900

    start challenge #27133 Square

---
+++ solutions/medium/27133-square/solution.ts
@@ -0,0 +1,25 @@
+/*
+ * 27133 - square
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type Square<N extends number> = number
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Square<0>, 0>>,
+  Expect<Equal<Square<1>, 1>>,
+  Expect<Equal<Square<3>, 9>>,
+  Expect<Equal<Square<20>, 400>>,
+  Expect<Equal<Square<100>, 10000>>,
+  Expect<Equal<Square<101>, 10201>>,
+
+  // Negative numbers
+  Expect<Equal<Square<-2>, 4>>,
+  Expect<Equal<Square<-5>, 25>>,
+  Expect<Equal<Square<-31>, 961>>,
+  Expect<Equal<Square<-50>, 2500>>,
+]

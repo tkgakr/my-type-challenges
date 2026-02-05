c1f56c52c8f99ae5d1a961527b23e1c8458b2449
Akira Takagi <tkgakrwork@gmail.com>
2026-02-05 21:05:02 +0900

    start challenge #27152 Triangular number

---
+++ solutions/medium/27152-triangular-number/solution.ts
@@ -0,0 +1,20 @@
+/*
+ * 27152 - triangular-number
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type Triangular<N extends number> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<Triangular<0>, 0>>,
+  Expect<Equal<Triangular<1>, 1>>,
+  Expect<Equal<Triangular<3>, 6>>,
+  Expect<Equal<Triangular<10>, 55>>,
+  Expect<Equal<Triangular<20>, 210>>,
+  Expect<Equal<Triangular<55>, 1540>>,
+  Expect<Equal<Triangular<100>, 5050>>,
+]

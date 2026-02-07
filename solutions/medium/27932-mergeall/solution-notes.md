12670763171d39cabbf04e83eec0eeb60f8b9c87
Akira Takagi <tkgakrwork@gmail.com>
2026-02-07 16:22:57 +0900

    end challenge #27932 MergeAll

--- solutions/medium/27932-mergeall/solution-notes.md
+++ solutions/medium/27932-mergeall/solution-notes.md
@@ -10,34 +10,68 @@
 ## 解法
 
 ### アプローチ
-<!-- ここに解法のアプローチを記述 -->
+
+2つのアプローチで実装した。
+
+#### 解法1: 再帰によるアキュムレータ合成
+
+タプルを先頭から順に処理し、アキュムレータ `Result` に `Merge` で合成していく再帰アプローチ。
+
+```ts
+type MergeAll<XS extends object[], Result extends object = {}> =
+  XS extends [infer First, ...infer Rest extends object[]]
+    ? MergeAll<Rest, Merge<Result, First>>
+    : Result
+```
+
+#### 解法2: ユニオン型への変換
+
+タプルをユニオン型 `XS[number]` に変換し、分配条件型を使って全キーを抽出。各キーに対して該当する値をユニオンで合成する非再帰アプローチ。
+
+```ts
+type MergeAll2<
+  XS extends object[],
+  U = XS[number],
+  Keys extends PropertyKey = U extends U ? keyof U : never
+> = {
+  [K in Keys]: U extends U ? U[K & keyof U] : never
+}
+```
 
 ### 実装のポイント
-<!-- 重要な実装ポイントを記述 -->
+
+- **Merge 型**: 2つのオブジェクトを合成する補助型。同じキーがある場合はユニオン型 `F[K] | S[K]` で合成
+- **分配条件型**: `U extends U ? keyof U : never` で各オブジェクトのキーを分配して取り出す
+- **インデックスアクセスの安全化**: `U[K & keyof U]` で `K` が `U` のキーでない場合も安全にアクセス
 
 ## 使用した型機能
 
-- [ ] Generics
-- [ ] Conditional Types (`T extends U ? X : Y`)
+- [x] Generics
+- [x] Conditional Types (`T extends U ? X : Y`)
 - [ ] Template Literal Types
-- [ ] Mapped Types (`{ [K in keyof T]: ... }`)
-- [ ] Type Inference (`infer`)
-- [ ] Recursive Types
+- [x] Mapped Types (`{ [K in keyof T]: ... }`)
+- [x] Type Inference (`infer`)
+- [x] Recursive Types
 - [ ] Utility Types
-- [ ] Index Access Types
-- [ ] Union Types
+- [x] Index Access Types
+- [x] Union Types
 - [ ] Intersection Types
 
 ## 学習メモ
 
 ### 新しく学んだこと、再確認したこと
-<!-- 新しい発見や学びを記述 -->
+
+- `XS[number]` でタプル型をユニオン型に変換できる
+- 分配条件型 `U extends U ? ... : never` を使うと、ユニオンの各要素に対して処理を適用できる
+- `K & keyof U` のようなインターセクションを使うと、型安全にインデックスアクセスできる
 
 ### つまずいたポイント
-<!-- 難しかった部分とその解決方法 -->
+
+- 解法2で全キーを取り出す際、単純に `keyof U` とするとユニオン型の共通キーしか取れない。分配条件型を使う必要がある
 
 ### 参考リンク
-<!-- 参考にした資料のリンク -->
+
+- [#599 Merge](https://github.com/type-challenges/type-challenges/blob/main/questions/00599-medium-merge/README.ja.md)
 
 ---
 *Generated at 2026-02-07 15:33:11*
--- solutions/medium/27932-mergeall/solution.ts
+++ solutions/medium/27932-mergeall/solution.ts
@@ -4,7 +4,35 @@
  */
 
 /* _____________ Your Code Here _____________ */
-type MergeAll<XS> = any
+type Merge<F, S> = {
+  // K は F と S の key を合わせたもの
+  [K in keyof F | keyof S]:
+    K extends keyof F
+      ? K extends keyof S
+        // F と S 両方のプロパティ
+        ? F[K] | S[K]
+        // F だけのプロパティ
+        : F[K]
+      : K extends keyof S
+        // S だけのプロパティ
+        ? S[K]
+        : never
+}
+// 解法1: 再帰によってアキュムレータにオブジェクトを合成するアプローチ
+type MergeAll<XS extends object[], Result extends object = {}> =
+  XS extends [infer First, ...infer Rest extends object[]]
+    ? MergeAll<Rest, Merge<Result, First>>
+    : Result
+
+// 解法2: XS を オブジェクトのユニオン型に置き換えて、再帰を使わずに合成するアプローチ
+type MergeAll2<
+  XS extends object[],
+  U = XS[number], // XS を オブジェクトのユニオンに変換
+  Keys extends PropertyKey = U extends U ? keyof U : never // Uにあるオブジェクトの全てのプロパティキーを取り出す(U を分配し、それぞれのオブジェクトキーを取り出してユニオンで束ねる)
+> = {
+  // 全てのキーにたいし、各オブジェクトに該当するキーがあればプロパティをユニオンで合成する
+  [K in Keys]: U extends U ? U[K & keyof U] : never
+}
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
@@ -35,4 +63,32 @@ type cases = [
   Expect<Equal<MergeAll<[{ a: 1 }, { a: number }]>, { a: number }>>,
   Expect<Equal<MergeAll<[{ a: number }, { a: 1 }]>, { a: number }>>,
   Expect<Equal<MergeAll<[{ a: 1 | 2 }, { a: 1 | 3 }]>, { a: 1 | 2 | 3 }>>,
+]
+
+type cases2 = [
+  Expect<Equal<MergeAll2<[]>, {} >>,
+  Expect<Equal<MergeAll2<[{ a: 1 }]>, { a: 1 }>>,
+  Expect<Equal<
+    MergeAll2<[{ a: string }, { a: string }]>,
+    { a: string }
+>
+  >,
+  Expect<Equal<
+    MergeAll2<[{ }, { a: string }]>,
+    { a: string }
+>
+  >,
+  Expect<Equal<
+    MergeAll2<[{ a: 1 }, { c: 2 }]>,
+    { a: 1, c: 2 }
+>
+  >,
+  Expect<Equal<
+    MergeAll2<[{ a: 1, b: 2 }, { a: 2 }, { c: 3 }]>,
+    { a: 1 | 2, b: 2, c: 3 }
+>
+  >,
+  Expect<Equal<MergeAll2<[{ a: 1 }, { a: number }]>, { a: number }>>,
+  Expect<Equal<MergeAll2<[{ a: number }, { a: 1 }]>, { a: number }>>,
+  Expect<Equal<MergeAll2<[{ a: 1 | 2 }, { a: 1 | 3 }]>, { a: 1 | 2 | 3 }>>,
 ]

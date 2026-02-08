978cb028d6e64dd989fe83415c733679d771ea5f
Akira Takagi <tkgakrwork@gmail.com>
2026-02-08 14:15:12 +0900

    start challenge #27958 CheckRepeatedTuple

---
+++ solutions/medium/27958-checkrepeatedtuple/solution.ts
@@ -0,0 +1,21 @@
+/*
+ * 27958 - checkrepeatedtuple
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type CheckRepeatedTuple<T extends unknown[]> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<CheckRepeatedTuple<[number, number, string, boolean]>, true>>,
+  Expect<Equal<CheckRepeatedTuple<[number, string]>, false>>,
+  Expect<Equal<CheckRepeatedTuple<[1, 2, 3]>, false>>,
+  Expect<Equal<CheckRepeatedTuple<[1, 2, 1]>, true>>,
+  Expect<Equal<CheckRepeatedTuple<[]>, false>>,
+  Expect<Equal<CheckRepeatedTuple<string[]>, false>>,
+  Expect<Equal<CheckRepeatedTuple<[number, 1, string, '1', boolean, true, false, unknown, any]>, false>>,
+  Expect<Equal<CheckRepeatedTuple<[never, any, never]>, true>>,
+]

25b0e2706156e32e66e65c8e350f31be526fc252
Akira Takagi <tkgakrwork@gmail.com>
2026-02-09 21:35:02 +0900

    start challenge #28333 PublicType

---
+++ solutions/medium/28333-public-type/solution.ts
@@ -0,0 +1,20 @@
+/*
+ * 28333 - public-type
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type PublicType<T extends object> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<PublicType<{ a: number }>, { a: number }>>,
+  Expect<Equal<PublicType<{ _b: string | bigint }>, {}>>,
+  Expect<Equal<PublicType<{ readonly c?: number }>, { readonly c?: number }>>,
+  Expect<Equal<PublicType<{ d: string, _e: string }>, { d: string }>>,
+  Expect<Equal<PublicType<{ _f: () => bigint[] }>, {}>>,
+  Expect<Equal<PublicType<{ g: '_g' }>, { g: '_g' }>>,
+  Expect<Equal<PublicType<{ __h: number, i: unknown }>, { i: unknown }>>,
+]

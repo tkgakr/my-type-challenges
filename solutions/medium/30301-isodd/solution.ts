38b64a80acd1a8d214e6bf27ed6bdf8d2344a34f
Akira Takagi <tkgakrwork@gmail.com>
2026-02-12 21:22:12 +0900

    start challenge #30301 IsOdd

---
+++ solutions/medium/30301-isodd/solution.ts
@@ -0,0 +1,21 @@
+/*
+ * 30301 - isodd
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type IsOdd<T extends number> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<IsOdd<5>, true>>,
+  Expect<Equal<IsOdd<2023>, true>>,
+  Expect<Equal<IsOdd<1453>, true>>,
+  Expect<Equal<IsOdd<1926>, false>>,
+  Expect<Equal<IsOdd<2.3>, false>>,
+  Expect<Equal<IsOdd<3e23>, false>>,
+  Expect<Equal<IsOdd<3e0>, true>>,
+  Expect<Equal<IsOdd<number>, false>>,
+]

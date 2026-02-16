9110a1a5f0ea835f200014936d553d7e06ee05a7
Akira Takagi <tkgakrwork@gmail.com>
2026-02-16 22:41:49 +0900

    start challenge #34007 compare-array-length

---
+++ solutions/medium/34007-compare-array-length/solution.ts
@@ -0,0 +1,17 @@
+/*
+ * 34007 - compare-array-length
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type CompareArrayLength<T extends any[], U extends any[]> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<CompareArrayLength<[1, 2, 3, 4], [5, 6]>, 1>>,
+  Expect<Equal<CompareArrayLength<[1, 2], [3, 4, 5, 6]>, -1>>,
+  Expect<Equal<CompareArrayLength<[], []>, 0>>,
+  Expect<Equal<CompareArrayLength<[1, 2, 3], [4, 5, 6]>, 0>>,
+]

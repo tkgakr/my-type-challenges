2d4c3f9a775aadf3c8d48fd05d648648ae3eb9be
Akira Takagi <tkgakrwork@gmail.com>
2026-02-18 22:23:53 +0900

    start challenge #35045 longest-common-prefix

---
+++ solutions/medium/35045-longest-common-prefix/solution.ts
@@ -0,0 +1,26 @@
+/*
+ * 35045 - longest-common-prefix
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type LongestCommonPrefix<T extends string[], P extends string = ''> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<LongestCommonPrefix<['flower', 'flow', 'flight']>, 'fl'>>,
+  Expect<Equal<LongestCommonPrefix<['dog', 'racecar', 'race']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['', '', '']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['a', '', '']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['', 'a', '']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['', '', 'a']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['a', 'a', '']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['a', '', 'a']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['', 'a', 'a']>, ''>>,
+  Expect<Equal<LongestCommonPrefix<['a', 'a', 'a']>, 'a'>>,
+  Expect<Equal<LongestCommonPrefix<['abc', 'abcd', 'abcde']>, 'abc'>>,
+  Expect<Equal<LongestCommonPrefix<[' ', ' ', ' ']>, ' '>>,
+  Expect<Equal<LongestCommonPrefix<['type-challenges', 'type-hero', 'typescript']>, 'type'>>,
+]

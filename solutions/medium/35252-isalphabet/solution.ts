9519725570bf706b36110575cc291b35388bca4e
Akira Takagi <tkgakrwork@gmail.com>
2026-02-20 21:50:36 +0900

    start challenge #35252 isalphabet

---
+++ solutions/medium/35252-isalphabet/solution.ts
@@ -0,0 +1,19 @@
+/*
+ * 35252 - isalphabet
+ * Difficulty: medium
+ */
+
+/* _____________ Your Code Here _____________ */
+type IsAlphabet<S extends string> = any
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<IsAlphabet<'A'>, true>>,
+  Expect<Equal<IsAlphabet<'z'>, true>>,
+  Expect<Equal<IsAlphabet<'9'>, false>>,
+  Expect<Equal<IsAlphabet<'!'>, false>>,
+  Expect<Equal<IsAlphabet<'😂'>, false>>,
+  Expect<Equal<IsAlphabet<''>, false>>,
+]
